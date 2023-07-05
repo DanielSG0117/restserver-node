d7fb639d8e9bb42b64617cd65495dcb978de7da8
Daniel Suarez <suarezgonzdaniel@gmail.com>
2023-07-05 16:48:05 -0600

    Backend basico

---
+++ models/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios'
+
+        //Middlewares: son funciones que siempre van a ejecutarse siempre que se levante el servidor
+        this.middlewares();
+
+
+        //Rutas de mi aplicación
+        this.routes();
+    }
+
+    middlewares() {
+
+        // CORS
+        this.app.use(cors());
+
+        //Lectura y parseo del body
+        this.app.use( express.json());
+
+        //Directorio publico
+        this.app.use(express.static('public'));
+
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath, require('../routes/userios'))
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo el puerto', this.port)
+        });
+
+    }
+
+}
+
+module.exports = Server 
