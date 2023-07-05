7559c9aa01822e256dfd914981dfea5a2564358c
Daniel Suarez Gonzalez <138052812+DanielSG0117@users.noreply.github.com>
2023-07-05 17:33:51 -0600

    Default port updated

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios'
 
         //Middlewares: son funciones que siempre van a ejecutarse siempre que se levante el servidor
